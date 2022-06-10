6afa92d07695d84703f501818d361db7f43ff957
lw <lw>
2022-06-10 18:36:43 +0800

    add npm

--- start_vue.sh
+++ start_vue.sh
@@ -8,5 +8,10 @@ sh /var/code/lw_cron/kill_vue.sh
 cd /var/code/html/vueblog-vue
 
 /usr/bin/npm install --save github-markdown-css
+npm i
+npm install --save github-markdown-css
+npm install element-ui --save
+npm install axios --save
+npm run serve
 
 nohup /usr/bin/npm run serve
