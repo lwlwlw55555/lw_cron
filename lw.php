dcd135fce1c8340ac40e15b3a49c355f7853ae59
lw <lw>
2022-06-17 18:22:51 +0800

    add return

--- lw.php
+++ lw.php
@@ -2,12 +2,18 @@
 require("includes/init.php");
 require(ROOT_PATH . "includes/erp_report/erp_report_function.php");
 
-var_dump($_REQUEST);
+// var_dump($_REQUEST);
 
-var_dump($_POST);
+// var_dump($_POST);
 
-var_dump($_GET);
+// var_dump($_GET);
 
-$datas = file_get_contents("php://input");
+// $datas = file_get_contents("php://input");
 
-var_dump($datas);
+// var_dump($datas);
+
+echo json_encode($_REQUEST);
+
+// curl -X POST -H 'content-type: application/x-www-form-urlencoded' -d 'key1=val1&key2=val2&key3=val3' "http://121.40.113.153/lw.php"
+// curl -X POST -H "Content-Type: application/json" -d '{"facility_id": 5007350}'  "http://121.40.113.153/lw.php"
+// curl -X GET "http://121.40.113.153/lw.php?lw=lw"
