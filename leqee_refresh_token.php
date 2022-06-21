c8a161d88b1feb506e6d8cde9c4ced0d3ef62fb2
lw <lw>
2022-06-21 16:18:56 +0800

    fix

--- leqee_refresh_token.php
+++ leqee_refresh_token.php
@@ -6,9 +6,9 @@ require("Services/LeqeeDbService.php");
 use Services\LeqeeDbService;
 
 if (empty($_REQUEST) || !array_key_exists('leqee_token', $_REQUEST) || !array_key_exists('gyc_token', $_REQUEST)) {
-	return json_encode(['code'=>1,'data'=>'error params']);
+	echo json_encode(['code'=>1,'data'=>'error params']);
 }
 
 LeqeeDbService::refreshLeqeeToken($_REQUEST['leqee_token'],$_REQUEST['gyc_token']);
 
-return json_encode(['code'=>0,'data'=>null]);
+echo json_encode(['code'=>0,'data'=>null]);
