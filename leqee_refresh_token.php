ec022a5348bfe31b1fedd9a12f5e51ca2cc1c2c7
lw <lw>
2022-06-22 10:38:22 +0800

    fix

--- leqee_refresh_token.php
+++ leqee_refresh_token.php
@@ -8,7 +8,7 @@ use Services\LeqeeDbService;
 global $db;
 
 if (array_key_exists('is_search', $_REQUEST) && $_REQUEST['is_search']) {
-	return json_encode(['code'=>0,'data'=>json_decode($db->getOne("select config_value from common_config where config_key = 'TOKEN_MAPPING'")]);
+	return json_encode(['code'=>0,'data'=>json_decode($db->getOne("select config_value from common_config where config_key = 'TOKEN_MAPPING'"))]);
 }
 
 if (empty($_REQUEST) || !array_key_exists('leqee_token', $_REQUEST) || !array_key_exists('gyc_token', $_REQUEST)) {
