417b34f8e6ff2d164c503d3e2604269a515fd04b
lw <lw>
2022-10-27 11:17:33 +0800

    revert dp_restart

--- dp_restart.sh
+++ dp_restart.sh
@@ -7,15 +7,16 @@ echo 'begin kill 8199'
 
 # lsof -i:8199 | awk '{print $2}' | grep -v PID xargs kill -9
 
-thread=$(lsof -i:8018 | awk '{print $2}' | grep -v PID)
+thread=$(lsof -i:8199 | awk '{print $2}' | grep -v PID | wc -l)
 echo $thread
-if [ $thread ]
+#这里中括号[]前后都要有空格啊啊啊啊啊！不然会报错的！！
+if [ $thread -gt 0 ]
 then
-    kill -9 $thread
+    lsof -i:8199 | awk '{print $2}' | grep -v PID | xargs kill -9
 else
     echo 'no 8199 thread'
 fi
 
 
-# nohup /usr/java/jdk1.8.0_311/bin/java -Xmx512m -Xms256m -Xss256k -jar /opt/bi.jar &
-nohup /usr/java/jdk1.8.0_311/bin/java -Xmx512m -Xms256m -Xss256k -jar /opt/do_com_admin.jar &
+nohup /usr/java/jdk1.8.0_311/bin/java -Xmx512m -Xms256m -Xss256k -jar /opt/bi.jar &
+# nohup /usr/java/jdk1.8.0_311/bin/java -Xmx512m -Xms256m -Xss256k -jar /opt/do_com_admin.jar &
