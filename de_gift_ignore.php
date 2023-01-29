f6a49dacd893d57fb280b91339d6551e913cb7d9
lw <lw>
2023-01-29 15:31:09 +0800

    [fix] DE电商-赠品忽略编码

--- de_gift_ignore.php
+++ de_gift_ignore.php
@@ -9,7 +9,8 @@ if (isset($_REQUEST['outerIdList']) && !empty($_REQUEST['outerIdList'])) {
 	echo json_encode(['code'=>0,'data'=>'']);
 	return;
 }if (isset($_REQUEST['is_export']) && $_REQUEST['is_export']) {
-    $res = getNormalAttachmentOut($result,'DE电商-赠品忽略编码');
+    $outerIdListStr = $redis->get('outerIdList');
+    $res = getNormalAttachmentOut(json_decode($outerIdListStr,true),'DE电商-赠品忽略编码');
 }else{
 	$outerIdListStr = $redis->get('outerIdList');
 	echo json_encode(['code'=>0,'data'=>['outerIdList'=>json_decode($outerIdListStr,true)]]);
